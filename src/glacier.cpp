1f8a3a81a35fe292ce7c18ca3e3d10041b42ae92
ItzSwiftyBoy <mizanalsabbih@gmail.com>
2024-05-15 17:52:27 +0600

    Added many stuffs I am lazy to describe.

--- src/glacier.cpp
+++ src/glacier.cpp
@@ -1,15 +1,6 @@
-#include <Compiler/Lexer.h>
 #include <iostream>
 
 int main() {
-  std::string_view content = "var x = 5";
-  Lexer lexer(content);
-  std::vector<Token> tokens = lexer.tokenize();
-
-  for (auto &token : tokens) {
-#if DEBUG_MODE == 1
-    std::cout << "Type = " << token.toString() << ", Lexeme = " << token.lexeme << ", Position = " << token.position.line << ":" << token.position.column << ";\n\n";
-#endif
-  }
+  std::cout << "Hi Glacier!" << "\n";
   return 0;
 }
--- src/utility.cpp
+++
@@ -1,9 +0,0 @@
-#include <utility.h>
-
-void Cursor::consume() {
-  if (position.index < contentLength()) {
-    position.index++;
-    position.column++;
-    currentChar = content[position.index];
-  }
-}
