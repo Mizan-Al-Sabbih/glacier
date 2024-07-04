0547dd5868637a1026ca65e8f8c18ea87113ec74
ItzSwiftyBoy <itzswiftyboy@gmail.com>
2024-07-04 15:52:16 +0600

    Again, committing for debugging.

--- src/glacier.cpp
+++ src/glacier.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
-#include <string_view>
 #include <memory>
+#include <string_view>
 
-#include "utility.h"
-#include "lexer.h"
-#include "ast.h"
+#include "compiler.h"
 
 int main() {
   std::string_view input = "var x = 5 \n";
   Compiler::Lexer lexer(input);
   auto tokens = lexer.analyze();
   for (const auto &token : tokens) {
-    std::cout << "Token_ID_Number: " << static_cast<int>(token.type) << " | Span: [" << token.span.start << ", " << token.span.end << "]\n";
-  }
-
-  Compiler::AST ast;
-  auto num_literal = std::make_shared<Compiler::NumberLiteral>(42);
-  auto var_decl = std::make_shared<Compiler::VariableDeclaration>("x", num_literal);
-
-  ast.add_node(var_decl);
-
-  const auto &nodes = ast.get_nodes();
-  for (const auto &node : nodes) {
-    if (auto var = std::dynamic_pointer_cast<Compiler::VariableDeclaration>(node)) {
-      std::cout << "\nVariable Declaration: " << var->name << " = ";
-      if (auto num = std::dynamic_pointer_cast<Compiler::NumberLiteral>(var->initializer)) {
-        std::cout << num->value << "\n";
-      }
-    }
+    std::cout << "Token_ID_Number: " << static_cast<int>(token.type)
+              << " | Span: [" << token.span.start << ", " << token.span.end
+              << "]\n";
   }
   std::cout << "Process ended";
   return 0;
